cf1b3d48d1d7266ce53d0c2a35b9b531ae6ee6eb
Brad Traversy <ajpDrumm@gmail.com>
2020-11-01 18:22:14 -0500

    Feature: setup calc with subtraction

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,14 +1,19 @@
 def addition(a,b):
     return a + b
 
+def subtraction(a,b):
+    return a - b
+
 class Calculator:
 
     def __init__(self):
-        x = 2 + 2
-        self.result = x;
         pass
 
     def add(self, a, b):
-        self.result = a + b
-        return addition(a, b)
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
 
--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -9,12 +9,17 @@ class MyTestCase(unittest.TestCase):
 
     def test_results_property_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(calculator.result, 0)
 
     def test_add_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.add(2, 2), 4)
         self.assertEqual(calculator.result, 4)
+
+    def test_subtract_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(2, 2), 0)
+        self.assertEqual(calculator.result, 0)
 
 if __name__ == '__main__':
     unittest.main()
