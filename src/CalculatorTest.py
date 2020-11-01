dfde84889873cc422c482f6180f96f89822f2e32
Brad Traversy <ajpDrumm@gmail.com>
2020-11-01 17:56:42 -0500

    BugFix: fixed Calulator Test errors

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -5,15 +5,16 @@ class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
         calculator = Calculator()
-        self.assertIsInstance(self.calculator, Calculator)
+        self.assertIsInstance(calculator, Calculator)
 
     def test_results_property_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.result, 4)
 
     def test_add_method_calculator(self):
-        calculator = Calculator
+        calculator = Calculator()
         self.assertEqual(calculator.add(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
 
 if __name__ == '__main__':
     unittest.main()
