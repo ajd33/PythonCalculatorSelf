4429cbe5b3bb1cb30162f094291cebd19b93e5e1
Brad Traversy <ajpDrumm@gmail.com>
2020-11-04 15:19:39 -0500

    Task: added copy of csv files to tests/data

---
+++ Tests/Data/Calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from Calculator import Calculator
+from CsvReader import CsvReader
+from pprint import pprint
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_subtraction(self):
+        test_data = CsvReader('/src/subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
