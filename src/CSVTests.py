485bc540007955501f6f77aa31a270f6b9b1ded8
Brad Traversy <ajpDrumm@gmail.com>
2020-11-08 09:18:03 -0500

    Fix: fix CSVTests.py

--- src/CSVTests.py
+++ src/CSVTests.py
@@ -1,19 +1,30 @@
 import unittest
-from CsvReader import CsvReader, ClassFactory
-from pprint import pprint
+from CsvReader.CsvReader import CsvReader, class_factory
 
 
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.csv_reader = CsvReader('employee_birthday.txt')
-
     def test_return_data_as_objects(self):
-        people = self.csv_reader.return_data_as_objects('person')
-        self.assertIsInstance(people, list)
-        test_class = ClassFactory('person', self.csv_reader.data[0])
-        for person in people:
-            self.assertEqual(person.__name__, test_class.__name__ )
+        value1 = self.csv_reader.return_data_as_class('Value 1')
+        value2 = self.csv_reader.return_data_as_class('Value 2')
+        result = self.csv_reader.return_data_as_class('Result')
+
+        self.assertIsInstance(value1, list)
+        self.assertIsInstance(value2, list)
+        self.assertIsInstance(result, list)
+
+        test_class_column1 = class_factory('Value 1', self.csv_reader.data[0])
+        test_class_column2 = class_factory('Value 2', self.csv_reader.data[0])
+        test_class_column3 = class_factory('result', self.csv_reader.data[0])
+
+        for value in value1:
+            self.assertEqual(value.__name__, test_class_column1.__name__)
+
+        for value in value2:
+            self.assertEqual(value.__name__, test_class_column2.__name__)
+
+        for value in result:
+            self.assertEqual(value.__name__, test_class_column3.__name__)
 
 
 if __name__ == '__main__':
